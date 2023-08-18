435061f16e798faf1f17e7ee7312ed2c9728f96f
rohan <khem7869218659@gmail.com>
2023-08-18 15:56:59 +0530

    navbar conectivity

--- src/component/Resturent.js
+++ src/component/Resturent.js
@@ -3,11 +3,26 @@ import "./style.css"
 import Menu from './Menuapi.js'
 import MenuCard from './MenuCard'
 const Resturent = () => {
+    
 
    const [menuData,setmenuData]= useState(Menu);
-   
+   const filterItem= (category)=>{
+     const updateList = Menu.filter( (curElem)=>{
+        return curElem.category === category;
+     });
+     setmenuData(updateList);
+   };
   return (
     <>
+      <nav className="navbar">
+        <div className="btn-group">
+            <button className="btn-group__item" onClick={()=>filterItem("breakfast")} >Breakfast</button>
+            <button className="btn-group__item" onClick={()=>filterItem("lunch")} >Lunch</button>
+            <button className="btn-group__item" onClick={()=>filterItem("evening")} >Evening</button>
+            <button className="btn-group__item" onClick={()=>filterItem("dinner")}  >Dinner</button>
+            <button className="btn-group__item" onClick={()=>setmenuData(Menu)}  >ALL</button>
+        </div>
+      </nav>
       <MenuCard menuData={menuData} />
       
     </>
